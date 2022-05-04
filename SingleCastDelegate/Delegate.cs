bd450cfd1a6e7b42b4a99fb86c2d05bb2a39eafd
mohan338 <sahumohan338@gmail.com>
2022-05-04 18:38:52 +0530

    [mohan]add:singlae cast element

---
+++ SingleCastDelegate/Delegate.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingleCastDelegate
+{
+    internal class Delegate
+    {
+        public delegate int data(int x, int y);
+        public static int Add(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
---
+++ SingleCastDelegate/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using SingleCastDelegate;
+using static SingleCastDelegate.Delegate;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        data add = new data(Add);
+        Console.WriteLine("addition of 2 numbers= " + add(20, 40));
+    }
+}
